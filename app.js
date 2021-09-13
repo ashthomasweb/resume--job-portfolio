187aee264bd323bdddcaef8cb4411a790601824a
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-09-13 12:39:07 -0400

    Lock file testing failure.
    Issue with npm module 'fs' throwing error over incorrect file system.
    Alternate solution specific to host to be tested next. No significant
    changes made in this merge.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
 // || Routes 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
